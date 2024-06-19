711b48704b14b8c1287ac0490ffe2c6c7fbeb478
ggumi030 <tndus50843@gmail.com>
2024-06-19 19:46:52 +0900

    init : init commit

---
+++ src/main/java/com/sparta/storyindays/StoryiNdaySApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.storyindays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryiNdaySApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoryiNdaySApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/storyindays/StoryiNdaySApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.storyindays;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoryiNdaySApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
