6ffaa4517644277abadc438ca5f76e018932821e
hsh1105 <hantg1105@naver.com>
2024-06-20 12:12:59 +0900

    feat : 프로필 조회

--- src/main/java/com/sparta/storyindays/entity/User.java
+++ src/main/java/com/sparta/storyindays/entity/User.java
@@ -4,9 +4,11 @@ import com.sparta.storyindays.dto.user.Auth;
 import com.sparta.storyindays.dto.user.State;
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
+@NoArgsConstructor
 @Table(name = "users")
 public class User extends Timstamped {
 
