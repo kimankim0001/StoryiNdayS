b12a28cd1833d50677c479e6c1607d524bb78cce
ggumi030 <tndus50843@gmail.com>
2024-06-20 12:15:12 +0900

    feat : 회원 가입 기능 구현
    - Request Enum 타입 처리
    - BusinessLogicException 추가
    - ExceptionCode 추가

--- src/main/java/com/sparta/storyindays/dto/user/Auth.java
+++ src/main/java/com/sparta/storyindays/dto/user/Auth.java
@@ -1,9 +1,19 @@
 package com.sparta.storyindays.dto.user;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+import com.sparta.storyindays.exception.BusinessLogicException;
+import com.sparta.storyindays.exception.ExceptionCode;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j(topic = "enum : Auth")
 public enum Auth {
     USER(UserType.USER),
     ADMIN(UserType.ADMIN);
 
+    @JsonValue
     private final String user;
 
     Auth(String user) {
@@ -17,5 +27,16 @@ public enum Auth {
     public static class UserType {
         public static final String USER = "USER";
         public static final String ADMIN = "ADMIN";
+    }
+
+    @JsonCreator
+    public static Auth from(String authType){
+        for(Auth auth : Auth.values()){
+            if(auth.getUser().equals(authType)){
+                return auth;
+            }
+        }
+        log.debug("EnumCollection.Auth.from() exception occur authType: {}", authType);
+        throw new BusinessLogicException(ExceptionCode.INVALID_AUTHTYPE.getCode());
     }
 }
