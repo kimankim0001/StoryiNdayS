ed08e9cc200b4b192d232514f1bdfc0ed666cad6
kinam <maltize9292@gmail.com>
2024-06-21 17:32:33 +0900

    refactor : 댓글 리스폰스 메서드 삭제

--- src/main/java/com/sparta/storyindays/dto/comment/CommentResDto.java
+++ src/main/java/com/sparta/storyindays/dto/comment/CommentResDto.java
@@ -1,9 +1,6 @@
 package com.sparta.storyindays.dto.comment;
 
-import com.sparta.storyindays.entity.Comment;
 import lombok.Getter;
-
-import java.time.LocalDateTime;
 
 @Getter
 public class CommentResDto {
@@ -14,20 +11,9 @@ public class CommentResDto {
 
     private String comment;
 
-    private LocalDateTime createdAt;
-
-    private LocalDateTime modifiedAt;
-
-    public CommentResDto(Long commentId, String username, String comment, LocalDateTime createdAt, LocalDateTime modifiedAt) {
+    public CommentResDto(Long commentId, String username, String comment) {
         this.commentId = commentId;
         this.username = username;
         this.comment = comment;
-        this.createdAt = createdAt;
-        this.modifiedAt = modifiedAt;
     }
-
-    public static CommentResDto toDto(Comment comment) {
-        return new CommentResDto(comment.getId(), comment.getUser().getUsername(), comment.getComment(), comment.getCreatedAt(), comment.getModifiedAt());
-    }
-
 }
