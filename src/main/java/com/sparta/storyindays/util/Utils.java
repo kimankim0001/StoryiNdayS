8d5ae2dbebf926f6c307b67ec5eef270b6ff2150
inho9979 <inho9979@naver.com>
2024-06-24 14:29:01 +0900

    fix : 코드 정리

--- src/main/java/com/sparta/storyindays/util/Utils.java
+++ src/main/java/com/sparta/storyindays/util/Utils.java
@@ -5,11 +5,14 @@ import org.springframework.data.domain.*;
 import java.util.List;
 
 public class Utils {
+
+    public final static int PAGE_SIZE = 5;
+
     public static Pageable getPageable(int page, boolean isAsc) {
         // 정렬방향, 정렬 기준(생성일자 고정), 페이저블 생성
         Sort.Direction direction = isAsc ? Sort.Direction.ASC : Sort.Direction.DESC;
         Sort sort = Sort.by(direction, "createdAt");
-        Pageable pageable = PageRequest.of(page, 5, sort);
+        Pageable pageable = PageRequest.of(page, PAGE_SIZE, sort);
 
         return pageable;
     }
